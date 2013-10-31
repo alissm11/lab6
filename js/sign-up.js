52c67dec7ed37b658074bf67f071e3c1db7ee998
unknown <iguest@is-m440m27-06.ischool.uw.edu>
2013-10-31 15:18:33 -0700

    Final submission for

--- js/sign-up.js
+++ js/sign-up.js
@@ -24,19 +24,33 @@ $(function(){
 
 	    //select a descendant input element with the name "addr-1"
 	    var addr1Input = signupForm.find('input[name="addr-1"]');
-	    var addr1Value = addrInput.val();
+	    var addr1Value = addr1Input.val();
 	    
-
+	    //code to check if address was added
+	    //if address was added, then needs to also add a zip
+	    //will alert user if zip was not added
 	    if (addr1Value && addr1Value.trim().length > 0) {
 	    	var zipInput = signupForm.find('input[name="zip"]');
 	    	var zipValue = zipInput.val(); 
-	    	if (zipValue && zipValue.trim().length > 0) {
+	    	if (zipValue && zipValue.trim().length > 0) 
 	    		return true;
-	    	} 
 	    	else {
 	    		alert("Please enter your zip code!"); 
 	    		return false; 
-	    	}
-    	}
+	    	} 
+	    }
+	});
+
+	$('.cancel-signup').click(function(){
+    	//code to run when user clicks "No Thanks!" button
+    	//show the modal confirmation dialog
+    	//and don't reset window.location until the user clicks
+    	//the "Yes, Get Me Out of Here!" button
+    	$('.cancel-signup-modal').modal();
+	}); //cancel-signup click
+
+	//add this at the end of your document ready function
+	$('.btn-abandon').click(function(){
+	    window.location = 'http://www.google.com';
 	});
 });
