f49b8797b2bc10d655849ea49c184f690c4b30f0
unknown <iguest@is-m440m27-03.ischool.uw.edu>
2013-10-31 13:22:34 -0700

    Started to begin lab, working on zip value and alert if not entered

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,3 +1,42 @@
 //signup.js 
 // add your JavaScript code to this file
 
+//doc ready function 
+//document is now ready for manipulation
+$(function(){
+	var stateSelect = $('.us-states[name="state"]');
+	var option; 
+	var idx;
+	var state;
+	for (idx = 0; idx < usStates.length; ++idx) {
+	    state = usStates[idx];
+	    option = $(document.createElement('option'));
+        option.attr('value', state.abbreviation);
+        option.html(state.name);
+        stateSelect.append(option);
+	}
+
+	$('.signup-form').submit(function(){
+	    //code to execute when the sign-up form is submitted
+	    //this is the raw DOM form element
+	    //wrap it in a jQuery object so we can use jQuery methods on it
+	    var signupForm = $(this);
+
+	    //select a descendant input element with the name "addr-1"
+	    var addr1Input = signupForm.find('input[name="addr-1"]');
+	    var addr1Value = addrInput.val();
+	    
+
+	    if (addr1Value && addr1Value.trim().length > 0) {
+	    	var zipInput = signupForm.find('input[name="zip"]');
+	    	var zipValue = zipInput.val(); 
+	    	if (zipValue && zipValue.trim().length > 0) {
+	    		return true;
+	    	} 
+	    	else {
+	    		alert("Please enter your zip code!"); 
+	    		return false; 
+	    	}
+    	}
+	});
+});
